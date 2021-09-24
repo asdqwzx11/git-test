90d2a8f788ce6208d11adf840d2117d5fb90f123
yushen <yushen@qq.com>
2021-09-24 15:06:31 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author yu
+ * @create 2021-09-24 15:02
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
